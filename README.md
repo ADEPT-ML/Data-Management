56597df9110aad6e9f3617c8428bbcce3348c824
Janis Büse <JanisBuese@gmail.com>
2022-09-05 17:40:03 +0200

    added basic configuration for buildings-api

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # Data-Management 🏭
 
-The service is responsible for managing all data files available to ADEPT.
+The service is responsible for managing all building data available to ADEPT.
 
 ## Requirements
 
 + Python >3.10
 + All packages from requirements.txt
+
+## Development
+
+### Local
+
+### Local
+
+Install dependencies from requirements.txt
+
+Start the service:
+
+```sh
+uvicorn main:app --reload
+```
 
 Copyright © ADEPT ML, TU Dortmund 2022
--- main.py
+++ main.py
@@ -2,14 +2,29 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src import importer
 
-from typing import Union
+data = dict()
 
-from fastapi import FastAPI
+
+def main():
+    global data
+    files = importer.fetch_files("data")
+    data = importer.parse_files(files)
+    importer.add_temperature_data(data, "data")
+
+
+main()
 
 app = FastAPI()
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+@app.get("/buildings")
+def read_buildings():
+    return {"buildings": [b.name for k, b in data.items()]}
